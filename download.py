50df6bd81d56e367065e8f857bfa1a7ab0a093c4
spea <spea55sugaku@outlook.jp>
2019-12-18 00:04:00 +0900

    convert mp4 file into mp3 file.

--- download.py
+++ download.py
@@ -1,20 +1,27 @@
 from pytube import YouTube
-import subprocess
 import os
 import glob
 import sys
+import subprocess
 
-url = sys.argv[1]
+url = sys.argv[1].strip()
 yt = YouTube(url)
 
 print('Start.')
-yt.streams.get_by_itag(140).download("C:/Users/626ca/IdeaProjects/SocketChat/src/spea55")
+yt.streams.get_by_itag(140).download("D:/goWork/SocketServer/MusicHolder")
 print('End.')
 
-dir = glob.glob("C:/Users/626ca/IdeaProjects/SocketChat/src/spea55/*.mp4")
-
+dir = glob.glob("D:/goWork/SocketServer/MusicHolder/*.mp4")
+print(dir)
 name = [n.split('\\')[-1] for n in dir]
+print(name)
 
-with open("C:/Users/626ca/IdeaProjects/SocketChat/src/spea55/links.txt", mode='a', encoding='UTF-8') as f:
+with open("D:/goWork/SocketServer/main/links.txt", mode='a', encoding='UTF-8') as f:
     f.write(url + ',' + name[0] + '\n')
     f.flush()
+
+os.chdir(r'C:\Program Files\ffmpeg-20191115-bfa8272-win64-static\bin')
+cmd = 'ffmpeg.exe -i  \"{0}\".mp4 \"{0}\".mp3'.format(str(dir[-1].split('.')[0]))
+
+subprocess.call(cmd)
+os.remove(dir[-1])
